7f5c242bd2cdb0acc29443ad9da99ee576bc1be9
dbolkovskis <denis.bolkovskis@ivolgamus.com>
2013-05-10 17:56:22 +0300

    Tutorial 1 done

--- NotepadCodeLab/Notepadv1/gen/com/android/demo/notepad1/R.java
+++ NotepadCodeLab/Notepadv1/gen/com/android/demo/notepad1/R.java
@@ -13,11 +13,16 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int text1=0x7f050000;
+    }
     public static final class layout {
         public static final int notepad_list=0x7f030000;
+        public static final int notes_row=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int menu_insert=0x7f040002;
         public static final int no_notes=0x7f040001;
     }
 }
